59cb46beb806cd4183435e83d58ddd8ee80c7896
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2016-09-21 05:18:17 +0200

    modified unigoogle utils

--- scripts/random_utils.py
+++ scripts/random_utils.py
@@ -44,106 +44,118 @@ def llnum2name(number):
   else:
     return '%d%c' %(number/10**good_base, num_map[good_base]);
       
-def lines_from_filehandle(filehandle):
+def lines_from_filehandle(filehandle, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   from itertools import islice;
   step_size = 0;
   while True:
-    buf_file = islice(filehandle, BUF_SIZE);
+    buf_file = islice(filehandle, batchsize);
     for line_count, line in enumerate(buf_file, start=1):
       yield line.strip();
-    print('(%s)'%(llnum2name(line_count+step_size*BUF_SIZE)), file=sys.stderr, end=' ');
-    if line_count < BUF_SIZE:
+    print('(%s)'%(llnum2name(line_count+step_size*batchsize)), \
+        file=sys.stderr, end=' ');
+    if line_count < batchsize:
       break;
     step_size += 1;
   return;
 
-def lines_from_filehandle__(filehandle):
+def lines_from_filehandle__(filehandle, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   for line_count, line in enumerate(filehandle, start=1):
     yield line.strip();
-    if not (line_count%BUF_SIZE):
+    if not (line_count%batchsize):
       print('(%s)'%(llnum2name(line_count)), file=sys.stderr, end=' ');
   print('(%s)'%(llnum2name(line_count)), file=sys.stderr);
   return;
 
-def lines_from_file(filename, large=False):
+def lines_from_file(filename, large=False, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   from itertools import islice;
   step_size = 0;
   with smart_open(filename, large=large) as infile:
     while True:
-      buf_file = islice(infile, BUF_SIZE);
+      buf_file = islice(infile, batchsize);
       for line_count, line in enumerate(buf_file, start=1):
         yield line.strip();
-      print('(%s)'%(llnum2name(line_count+step_size*BUF_SIZE)), file=sys.stderr, end=' ');
-      if line_count < BUF_SIZE:
+      print('(%s)'%(llnum2name(line_count+step_size*batchsize)), \
+          file=sys.stderr, end=' ');
+      if line_count < batchsize:
         break;
       step_size += 1;
   return;
 
-def lines_from_file__(filename, large=False):
+def lines_from_file__(filename, large=False, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   with smart_open(filename, large=large) as infile:
     for line_count, line in enumerate(infile, start=1):
       yield line.strip();
-      if not (line_count%BUF_SIZE):
+      if not (line_count%batchsize):
         print('(%s)'%(llnum2name(line_count)), file=sys.stderr, end=' ');
     print('(%s)'%(llnum2name(line_count)), file=sys.stderr);
   return;
 
-def lines_to_filehandle(filehandle, lines):
+def lines_to_filehandle(filehandle, lines, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   from itertools import islice;
   step_size = 0;
   while True:
-    buf_lines = islice(lines, BUF_SIZE);
+    buf_lines = islice(lines, batchsize);
     for line_count, sent in enumerate(buf_lines, start=1):
       print(sent.strip(), file=filehandle);
-    print('(%s)'%(llnum2name(line_count+step_size*BUF_SIZE)), file=sys.stderr, end=' ');
-    if line_count < BUF_SIZE:
+    print('(%s)'%(llnum2name(line_count+step_size*batchsize)), \
+        file=sys.stderr, end=' ');
+    if line_count < batchsize:
       break;
     step_size += 1;
   return True;
 
-def lines_to_filehandle__(filehandle, lines):
+def lines_to_filehandle__(filehandle, lines, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   for line_count, sent in enumerate(lines, start=1):
     print(sent.strip(), file=filehandle);
-    if not (line_count%BUF_SIZE): 
+    if not (line_count%batchsize): 
       print('(%s)'%(llnum2name(line_count)), file=sys.stderr, end=' ');
   print('(%s)'%(llnum2name(line_count)), file=sys.stderr);
   return True;
 
-def lines_to_file(filename, lines):
+def lines_to_file(filename, lines, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   from itertools import islice;
   step_size = 0;
   with smart_open(filename, mode='wb') as outfile:
     while True:
-      buf_lines = islice(lines, BUF_SIZE);
+      buf_lines = islice(lines, batchsize);
       for line_count, sent in enumerate(buf_lines, start=1):
         print(sent.strip(), file=outfile);
-      print('(%s)'%(llnum2name(line_count+step_size*BUF_SIZE)), file=sys.stderr, end=' ');
-      if line_count < BUF_SIZE:
+      print('(%s)'%(llnum2name(line_count+step_size*batchsize)), \
+          file=sys.stderr, end=' ');
+      if line_count < batchsize:
         break;
       step_size += 1;
   return True;
 
-def lines_to_file__(filename, lines):
+def lines_to_file__(filename, lines, batchsize=0):
   global BUF_SIZE;
+  batchsize = BUF_SIZE if not batchsize else batchsize;
   line_count = 0;
   with smart_open(filename, mode='wb') as outfile:
     for line_count, sent in enumerate(lines, start=1):
       print(sent.strip(), file=outfile);
-      if not (line_count%BUF_SIZE): 
+      if not (line_count%batchsize): 
         print('(%s)'%(llnum2name(line_count)), file=sys.stderr, end=' ');
     print('(%s)'%(llnum2name(line_count)), file=sys.stderr);
   return True;
@@ -151,10 +163,10 @@ def lines_to_file__(filename, lines):
 def encode_sentence(sentence, vocabulary, id_gen=itertools.count(1)):
   enc_repr = [];
   for token in re.split('\s+', sentence.strip()):
-    if vocabulary.has_key(token):
+    if token in vocabulary:
       enc_repr.append(vocabulary[token]);
     else:
-      vocab_idx = id_gen.next();
+      vocab_idx = next(id_gen);
       vocabulary[token] = vocab_idx;
       enc_repr.append(vocab_idx);
   return tuple(enc_repr);
--- scripts/unigoogle_utils.py
+++ scripts/unigoogle_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
+from __future__ import print_function;
 
 import multiprocessing, re, sys, time;
 
@@ -22,11 +22,15 @@ def convert(tag_sequence, mapping=None):
     mapping = map_tag;
   coarse_sequence = [];
   for idx, tag in enumerate(tag_sequence):
-    possible_matches = [key for key in filter(lambda X: X[0] == tag, mapping.keys())];
+    possible_matches = [key for key in \
+        filter(lambda X: X[0] == tag, mapping.keys())];
     if len(possible_matches) > 1 and idx > 0:
-      best_match = [key for key in filter(lambda X: X[1] == tag_sequence[idx-1], filter(lambda X: len(X) == 2, possible_matches) )];
+      best_match = [key for key in \
+          filter(lambda X: X[1] == tag_sequence[idx-1], \
+          filter(lambda X: len(X) == 2, possible_matches))];
       if len(best_match) < 1: 
-        best_match = [key for key in filter(lambda X: len(X) == 1, possible_matches)];
+        best_match = [key for key in \
+            filter(lambda X: len(X) == 1, possible_matches)];
       coarse_sequence.append( mapping[best_match[0]] );
     elif tag == '_UNK_' or len(possible_matches) == 0:
       coarse_sequence.append( 'X' );
@@ -45,7 +49,7 @@ def convert_tagged_text(*args):
   inputFileName  = args[1] if len(args) >= 2 else '';
   outputFileName = args[2] if len(args) >= 3 else '';
   delimold = delimnew = '_';
-  formsBuffer, tagsBuffer, bufferSize = [], [], 0;
+  formsBuffer, tagsBuffer, bufferSize = [], [], 100000;
 
   keepTags = True;
   threads = 2;
@@ -53,43 +57,39 @@ def convert_tagged_text(*args):
   if threads > 1:
     pool = multiprocessing.Pool(threads, maxtasksperchild=1000);
 
+  if keepTags:
+    outputWriter = lambda X, Y: \
+        ' '.join(["%sc%s" %(x, delimnew, y) for x, y in zip(X, Y)]);
+  else:
+    outputWriter = lambda X: ' '.join(["%s" %x for x in X]);
+
   oldtime, newtime = time.time(), time.time();
   with random_utils.smart_open(outputFileName, mode='wb') as outputFile:
-    for line in random_utils.lines_from_file(inputFileName):
-      tokens = [tuple(tok.rsplit(delimold, 1)) if tok.find(delimold) != -1 else (tok, '_UNK_') for tok in re.split('\s+', line.strip())];
-      current_forms = [tok[0] for tok in tokens];
-      formsBuffer.append(current_forms);
-      if keepTags:
-        current_tags = [tok[1] for tok in tokens];
-        tagsBuffer.append(current_tags);
-      bufferSize += 1;
-      if bufferSize == 100000:
-        if keepTags and threads > 1:
-          for current_forms, mapped_tags in izip(formsBuffer, pool.imap(convert, tagsBuffer, chunksize=10000)):
-            print(' '.join(['%s%c%s'%(tok, delimnew, tag) for tok, tag in zip(current_forms, mapped_tags)]), file=outputFile);
-        elif keepTags and threads <= 1:
-          for current_forms, mapped_tags in izip(formsBuffer, imap(convert, tagsBuffer)):
-            print(' '.join(['%s%c%s'%(tok, delimnew, tag) for tok, tag in zip(current_forms, mapped_tags)]), file=outputFile);
-        else:
-          for current_forms in formsBuffer:
-            print(' '.join(current_forms), file=outputFile);
-        formsBuffer, tagsBuffer, bufferSize = [], [], 0;
-        newtime = time.time();
-        print("Dumping tags in %.5f"%(newtime-oldtime), file=sys.stderr);
-        oldtime = newtime;
-    if bufferSize:
+    inputStream = random_utils.lines_from_file(inputFileName);
+    while True:
+      inputBuffer = islice(inputStream, bufferSize);
+      for line_count, line in inputBuffer:
+        tokens = [tuple(tok.rsplit(delimold, 1)) if tok.find(delimold) != -1 else (tok, '_UNK_') for tok in re.split('\s+', line.strip())];
+        current_forms = [tok[0] for tok in tokens];
+        formsBuffer.append(current_forms);
+        if keepTags:
+          current_tags = [tok[1] for tok in tokens];
+          tagsBuffer.append(current_tags);
       if keepTags and threads > 1:
-        for current_forms, mapped_tags in izip(formsBuffer, pool.imap(convert, tagsBuffer, chunksize=10000)):
-          print(' '.join(['%s%c%s'%(tok, delimnew, tag) for tok, tag in zip(current_forms, mapped_tags)]), file=outputFile);
+        outputBuffer = zip(formsBuffer, \
+            pool.imap(convert, tagsBuffer, chunksize=10000));
       elif keepTags and threads <= 1:
-        for current_forms, mapped_tags in izip(formsBuffer, imap(convert, tagsBuffer)):
-          print(' '.join(['%s%c%s'%(tok, delimnew, tag) for tok, tag in zip(current_forms, mapped_tags)]), file=outputFile);
+        outputBuffer = zip(formsBuffer, map(convert, tagsBuffer));
       else:
-        for current_forms in formsBuffer:
-          print(' '.join(current_forms), file=outputFile);
+        outputBuffer = formsBuffer;
+      random_utils.lines_to_filehandle(outputFile, \
+          map(outputWriter, outputBuffer));
+      if line_count < bufferSize:
+        break;
       formsBuffer, tagsBuffer, bufferSize = [], [], 0;
       newtime = time.time();
       print("Dumping tags in %.5f"%(newtime-oldtime), file=sys.stderr);
+      oldtime = newtime;
 
   if threads > 1:
     pool.close();
