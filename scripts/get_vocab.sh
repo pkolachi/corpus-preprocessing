d779e6813386bc4df512464c7aad2c772a508b37
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2016-10-18 07:43:58 +0200

    vocabulary from conll files

---
+++ scripts/get_vocab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TAB=`echo -e "\t"`
+
+bzcat $1 | grep -v "^$" | \
+    cut -f2,4 | LC_ALL="C" tr '[:upper:]' '[:lower:]' | \
+    LC_ALL="C" sort -k1 --parallel=8 -T "$PWD/tmp" | uniq -c | \
+    sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
+    sort -S8G -k1,1nr --stable -t"$TAB" --parallel=8 -T "$PWD/tmp" > $2.vcb
+
+cat $2.vcb | \
+    awk -F'\t' '
+{ 
+    if(key!=$2) {
+	print count"\t"key"\t"value; count=$1; key=$2; value=$3;
+    } else {
+	value=value":::"$3; count+=$1;
+    } 
+}' | \
+    sort -S8G -k1,1nr --stable -t"$TAB" --parallel=8 -T "$PWD/tmp" > $2.lexicon
+
