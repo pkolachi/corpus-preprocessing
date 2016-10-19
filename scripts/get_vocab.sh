c38b242abb8dca58c2944a62b2bc1e0a7340bba3
Prasanth Kolachina <prasanth.kolachina@cse.gu.se>
2016-10-19 10:29:13 +0200

    fix get_vocab. awk string number dealing

--- scripts/get_vocab.sh
+++ scripts/get_vocab.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 TAB=`echo -e "\t"`
-PARALLEL=""
+PARALLEL="--parallel=8"
 
 mkdir -p "$PWD/tmp";
- #bzcat $1 | grep -v "^$" | \
- #    cut -f2,4 | LC_ALL="C" tr '[:upper:]' '[:lower:]' | \
- #    LC_ALL="C" sort -k1"$PARALLEL" -T "$PWD/tmp" | uniq -c | \
- #    sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
- #    sort -S8G -k1,1nr"$PARALLEL" --stable -t"$TAB" -T "$PWD/tmp" > "$2.vcb"
+bzcat $1 | grep -v "^$" | \
+    cut -f2,4 | LC_ALL="C" tr '[:upper:]' '[:lower:]' | \
+    LC_ALL="C" sort -k1"$PARALLEL" -T "$PWD/tmp" | uniq -c | \
+    sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
+    sort -S8G -k1,1nr"$PARALLEL" --stable -t"$TAB" -T "$PWD/tmp" > "$2.vcb"
 
 cat "$2.vcb" | \
-    LC_ALL="C" sort -S8G -k2,3"$PARALLEL" --stable -t"$TAB" -T"$PWD/tmp" #| \
+    LC_ALL="C" sort -S8G -k2,3"$PARALLEL" --stable -t"$TAB" -T"$PWD/tmp" | \
     awk -F'\t' '
 { 
-    if(key!=$2) {
-	print count"\t"key"\t"value; count=$1; key=$2; value=$3;
+    if(key!=""$2) {
+	print count"\t"key"\t"value; count=$1; key=""$2; value=""$3;
     } else {
 	value=value":::"$3; count+=$1;
     } 
