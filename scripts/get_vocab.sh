7bb3501954cb32ea55d448181489fabaebf13b86
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2016-10-26 07:07:10 +0200

    extract moph lexicon

--- scripts/get_vocab.sh
+++ scripts/get_vocab.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 TAB=`echo -e "\t"`
-PARALLEL="--parallel=8"
+PARALLEL="--parallel=2"
 
 mkdir -p "$PWD/tmp";
-bzcat $1 | grep -v "^$" | \
+bzcat $1 | grep -v "^#" | grep -v "^$" | \
     cut -f2,4 | LC_ALL="C" tr '[:upper:]' '[:lower:]' | \
     LC_ALL="C" sort -k1"$PARALLEL" -T "$PWD/tmp" | uniq -c | \
     sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
-    sort -S8G -k1,1nr"$PARALLEL" --stable -t"$TAB" -T "$PWD/tmp" > "$2.vcb"
+    sort -S 50% -k1,1nr"$PARALLEL" --stable -t"$TAB" -T "$PWD/tmp" > "$2.vcb"
+
+bzcat $1 | grep -v "^#" | grep -v "^$" | \
+    cut -f2,3,4,6 | \
+    LC_ALL="C" sort -S 50% "$PARALLEL" -T "$PWD/tmp" | uniq -c | \
+    sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
+    LC_ALL="C" sort -S 50% "$PARALLEL" -k4,4 -k3,3 -k2,2 | \
+    awk '{print $1"\t"$4"\t"$3"\t"$2"\t"$5;}' > "$2.morph_lexicon"
 
 cat "$2.vcb" | \
     LC_ALL="C" sort -S8G -k2,3"$PARALLEL" --stable -t"$TAB" -T"$PWD/tmp" | \
@@ -20,6 +27,6 @@ cat "$2.vcb" | \
 	value=value":::"$3; count+=$1;
     } 
 }' | \
-    sort -S8G -k1,1nr"$PARALLEL" --stable -t"$TAB" -T "$PWD/tmp" > $2.lexicon
+    sort -S 50% -k1,1nr"$PARALLEL" --stable -t"$TAB" -T "$PWD/tmp" > "$2.lexicon"
 
 
