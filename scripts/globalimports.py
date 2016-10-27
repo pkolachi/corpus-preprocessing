7855af8bd9c4dd839c9033e2a2315e598b972048
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2016-10-27 19:08:13 +0200

    make float division default in globalimports

--- scripts/globalimports.py
+++ scripts/globalimports.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 
-from __future__ import print_function
+from __future__ import print_function, division;
 try:
   from itertools import imap as map, \
       ifilter as filter, \
