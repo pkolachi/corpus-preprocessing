324f3d790b0a06ba4e1ebd4def8d1197817e853a
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2017-06-24 20:30:22 +0200

    directory comparison; threaded

--- scripts/cmpDirectories.py
+++ scripts/cmpDirectories.py
@@ -7,11 +7,14 @@ try:
   from itertools import imap as map, izip as zip, ifilter as filter, repeat;
 except ImportError:
   from itertools import repeat;
+import multiprocessing as Threads;
 
 srcDirectory = sys.argv[1];
 tgtDirectory = sys.argv[2];
-srcDirectory = os.path.normpath(srcDirectory)
-tgtDirectory = os.path.normpath(tgtDirectory);
+srcDirectory = os.path.abspath(srcDirectory)
+tgtDirectory = os.path.abspath(tgtDirectory);
+
+cmpFilesList = [];
 
 for dirpath, dirList, fileList in os.walk(srcDirectory):
   tgtdirpath = dirpath.replace(srcDirectory, tgtDirectory);
@@ -19,12 +22,28 @@ for dirpath, dirList, fileList in os.walk(srcDirectory):
       (dirname.replace(srcDirectory, tgtDirectory) for dirname in dirList));
   for tgtdir in tgtdirsList:
     if os.path.isdir(tgtdir) == False:
-      print("%s: Target directory does not match" %(tgtdir));
+      print("%s: Target directory does not exist" %(tgtdir));
     
   tgtfilesList = map(os.path.join, repeat(tgtdirpath, len(fileList)), \
       (filename.replace(srcDirectory, tgtDirectory) for filename in fileList));
   srcfileList = map(os.path.join, repeat(dirpath, len(fileList)), fileList);
-  for srcfile, tgtfile in zip(srcfileList, tgtfilesList):
-    if os.path.isfile(tgtfile) == False or \
-        os.system('cmp "%s" "%s" > /dev/null 2> /dev/null' %(srcfile, tgtfile)) != 0:
-          print("%s: Target file does not match" %(tgtfile));
+  cmpFilesList.extend([(srcfile, tgtfile) for srcfile, tgtfile in zip(srcfileList, tgtfilesList)]);
+
+def Compare(args):
+  srcfile, tgtfile = args;
+  #print(srcfile+", "+tgtfile, file=sys.stderr);
+  if os.path.isfile(tgtfile) == False:
+    return "%s: Target file does not exist" %(tgtfile);
+  elif os.system('cmp "%s" "%s" > /dev/null 2> /dev/null' %(srcfile, tgtfile)) != 0:
+    return "%s: Target file does not match" %(tgtfile);
+  else:
+    return "";
+
+#pool = Threads.Pool(8);
+#cmpOutput = pool.map(Compare, cmpFilesList, chunksize=10000);
+#pool.close();
+#pool.join();
+
+cmpOutput = map(Compare, cmpFilesList);
+cmpOutput = filter(None, cmpOutput);
+print("\n".join(cmpOutput));
