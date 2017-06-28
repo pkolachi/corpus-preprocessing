a4eb1d9cd59898bea2589fd0a1b3dec7a6a0c484
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2017-06-28 15:09:51 +0200

    added buffer to get_vocab

--- scripts/get_vocab.sh
+++ scripts/get_vocab.sh
@@ -5,17 +5,34 @@ TAB=`echo -e "\t"`
 SORT_OPTS="-S 50% -T $PWD/tmp"
 export LC_ALL=C
 
-MORPH_TAGGED=true
-
-mkdir -p "$PWD/tmp";
+TMP="$PWD/tmp"
+if [[ ! -d "${TMP}" ]] ; then
+    mkdir -p "${TMP}";
+fi
 
 transformer1="tr '[:upper:]' '[:lower:]'"
 transformer2="awk '{print tolower($0)}'"
 
+MORPH_TAGGED=true
+if $MORPH_TAGGED ; then 
+    BUFFER="$TMP/$$.morphbuffer"
+    bzcat $1 | grep -v -e "^#" -e "^$" | \
+	cut -f2,3,4,6 | \
+	awk '{print tolower($1)"\t"$2"\t"$3"\t"$4;}' | \
+	head -n 1000000 \
+	> $BUFFER
+else
+    BUFFER="$TMP/$$.tagsbuffer"
+    bzcat $1 | grep -v -e "^#" -e "^$" | \
+	cut -f2,4 | \
+	awk '{print tolower($1)"\t"$2;}' | \
+	head -n 1000000 \
+	> $BUFFER
+fi
 
 # lower-cased surface forms
-bzcat $1 | grep -v -e "^#" -e "^$" | \
-    cut -f2,4 | awk '{print tolower($1)"\t"$2;}' | \
+cat $BUFFER | \
+    awk '{print $1"\t"$3;}' | \
     sort $SORT_OPTS -k1 | uniq -c | \
     sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
     sort $SORT_OPTS -k1,1nr --stable -t"$TAB" > "$2.vcb"
@@ -33,17 +50,16 @@ cat "$2.vcb" | \
     sort $SORT_OPTS -k1,1nr --stable -t"$TAB" > "$2.taglex"
 
 if $MORPH_TAGGED ; then
-    bzcat $1 | grep -v -e "^#" -e "^$" | \
-	cut -f3,4 | \
+    cat $BUFFER | cut -f2,3 | \
 	sort $SORT_OPTS -k1 | uniq -c | \
 	sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
 	sort $SORT_OPTS -k1,1nr --stable -t"$TAB" > "$2.lemmas"
     
-    bzcat $1 | grep -v -e "^#" -e "^$" | \
-	cut -f2,3,4,6 | awk '{print tolower($1)"\t"$2"\t"$3"\t"$4;}' | \
+    cat $BUFFER | \
 	sort $SORT_OPTS -k1 | uniq -c | \
 	sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
 	sort $SORT_OPTS -k4,4 -k3,3 -k2,2 | \
 	awk '{print $1"\t"$4"\t"$3"\t"$2"\t"$5;}' > "$2.morphlex"
 fi
 
+rm -v $BUFFER
