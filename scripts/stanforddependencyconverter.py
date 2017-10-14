d7802880d8c8a849e434175f529022f93d24a0d1
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2017-10-14 11:08:43 +0200

    sdeps converter changed

--- scripts/stanforddependencyconverter.py
+++ scripts/stanforddependencyconverter.py
@@ -13,16 +13,22 @@ except ImportError:
 #stanford_parser_dir = '/Users/prakol/Documents/softwares/nlp-tools/language-specific/english/stanford-tools/stanford-corenlp-full-2014-10-31';
 #stanford_parser_dir = '/Users/prakol/Documents/softwares/nlp-tools/language-specific/english/stanford-tools/stanford-corenlp-full-2013-11-12';
 #stanford_parser_dir = '/Users/prakol/Documents/softwares/nlp-tools/language-specific/english/stanford-tools/stanford-corenlp-full-2015-04-20';
-stanford_parser_dir = '/Users/prakol/Documents/softwares/nlp-tools/language-specific/english/stanford-tools/stanford-corenlp-full-2015-12-09';
+stanford_parser_dir = '/home/prakol/Documents/softwares/nlp-tools/language-specific/english/stanford-tools/stanford-corenlp-full-2015-12-09';
 
-#java -mx3000m -cp "%s/*:" edu.stanford.nlp.trees.EnglishGrammaticalStructure \
 dep_conversion_cmd = '''
-java -mx3000m -cp "%s/*:" edu.stanford.nlp.trees.international.pennchinese.ChineseGrammaticalStructure \
+/home/prakol/Documents/softwares/java/jre1.8.0_40/bin/java -mx3000m -cp "%s/*:" edu.stanford.nlp.trees.EnglishGrammaticalStructure \
     -basic \
     -keepPunct \
     -conllx \
     -nthreads %d \
     -treeFile ''' %(stanford_parser_dir, cpu_count());
+#dep_conversion_cmd = '''
+#/home/prakol/Documents/softwares/java/jre1.8.0_40/bin/java -mx3000m -cp "%s/*:" edu.stanford.nlp.trees.international.pennchinese.ChineseGrammaticalStructure \
+#    -basic \
+#    -keepPunct \
+#    -conllx \
+#    -nthreads %d \
+#    -treeFile ''' %(stanford_parser_dir, cpu_count());
 
 const_parse_file = sys.argv[1];
 tmpfile = '/tmp/%s.pid' %(os.getpid());
@@ -33,13 +39,13 @@ for parse in random_utils.lines_from_file(const_parse_file):
   if len(parsesBuf) > 100000:
     random_utils.lines_to_file(tmpfile, parsesBuf);
     cmd = '%s %s' %(dep_conversion_cmd, tmpfile);
-    if subprocess.call(shlex.split(cmd), stdout=sys.stdout, stderr=fnull) != 0:
+    if subprocess.call(shlex.split(cmd), stdout=sys.stdout, stderr=sys.stderr) != 0:
       sys.exit(1);
     parsesBuf = [];
 if len(parsesBuf):
   random_utils.lines_to_file(tmpfile, parsesBuf);
   cmd = '%s %s' %(dep_conversion_cmd, tmpfile);
-  if subprocess.call(shlex.split(cmd), stdout=sys.stdout, stderr=fnull) != 0:
+  if subprocess.call(shlex.split(cmd), stdout=sys.stdout, stderr=sys.stderr) != 0:
     sys.exit(1);
   parsesBuf = [];
 
