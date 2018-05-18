9e81104cd47985b24f5b3d3ca2c64ea2df12ba4e
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2018-05-18 12:38:18 +0200

    handle spaces in UDv2 in form and lemma fields

--- scripts/get_vocab.sh
+++ scripts/get_vocab.sh
@@ -6,8 +6,10 @@ if [[ ! -d "${TMP}" ]] ; then
 fi
 
 TAB=`echo -e "\t"`
-SORT_OPTS="-S 20% --parallel=4 -T $PWD/tmp";
-#SORT_OPTS="-S 20% -T $PWD/tmp"
+BINSORT="/usr/bin/sort"
+BINSORT="/usr/local/bin/gsort"
+SORTOPTS="-S 20% --parallel=4 -T $PWD/tmp";
+#SORTOPTS="-S 20% -T $PWD/tmp"
 export LC_ALL=C.UTF-8
 
 FRDR=""             # reader program for input file
@@ -49,32 +51,32 @@ eval $FRDR $1 | grep -v -e "^#" -e "^$" | \
 # lower-cased surface forms
 cat $BUFFER | \
   awk -F"$TAB" '{print $1"\t"$3;}' | \
-  sort $SORT_OPTS -k1 | uniq -c | \
-  sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
-  sort $SORT_OPTS -k1,1nr --stable -t"$TAB" > "$2.vcb"
+  $BINSORT $SORTOPTS -k1 | uniq -c | \
+  sed -e 's/^[ \t]*//g' -e $'s/^\([0-9]*\) /\1\t/g' | \
+  $BINSORT $SORTOPTS -k1,1nr --stable -t"$TAB" > "$2.vcb"
 
 cat "$2.vcb" | \
-  sort $SORT_OPTS -k2,3 --stable -t"$TAB" | \
+  $BINSORT $SORTOPTS -k2,3 --stable -t"$TAB" | \
   awk -F"$TAB" '{ 
     if(key!=""$2) {
-	print count"\t"key"\t"value; count=$1; key=""$2; value=""$3;
+        print count"\t"key"\t"value; count=$1; key=""$2; value=""$3;
     } else {
-	value=value":::"$3; count+=$1;
+        value=value":::"$3; count+=$1;
     } 
   }' | \
-  sort $SORT_OPTS -k1,1nr --stable -t"$TAB" > "$2.taglex"
+  $BINSORT $SORTOPTS -k1,1nr --stable -t"$TAB" > "$2.taglex"
 
 if $MORPH_TAGGED ; then
   cat $BUFFER | cut -f2,3 | \
-    sort $SORT_OPTS -k1 | uniq -c | \
-    sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
-    sort $SORT_OPTS -k1,1nr --stable -t"$TAB" > "$2.lemmas"
+    $BINSORT $SORTOPTS -k1 | uniq -c | \
+    sed -e 's/^[ \t]*//g' -e $'s/^\([0-9]*\) /\1\t/g' | \
+    $BINSORT $SORTOPTS -k1,1nr --stable -t"$TAB" > "$2.lemmas"
     
   cat $BUFFER | \
-    sort $SORT_OPTS -k1 | uniq -c | \
-    sed -e 's/^[ \t]*//g' -e $'s/ /\t/g' | \
-    sort $SORT_OPTS -k4,4 -k3,3 -k2,2 | \
+    $BINSORT $SORTOPTS -k1 | uniq -c | \
+    sed -e 's/^[ \t]*//g' -e $'s/^\([0-9]*\) /\1\t/g' | \
+    $BINSORT $SORTOPTS -k4,4 -k3,3 -k2,2 | \
     awk '{print $1"\t"$4"\t"$3"\t"$2"\t"$5;}' > "$2.morphlex"
 fi
 
-rm -v $BUFFER
+#rm -v $BUFFER
