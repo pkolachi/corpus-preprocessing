660d8d36f940800064286c6960edf3b79ae5dd0b
Prasanth Kolachina <prasanth.kolachina@cse.gu.se>
2019-02-08 11:38:02 +0100

    new version of the script for WikiDocExtract

--- scripts/WikiDocumentExtractorNew.py
+++ scripts/WikiDocumentExtractorNew.py
@@ -3,6 +3,7 @@
 #from globalimports import *;
 from globalimports import * ;
 import random_utils as ru ;
+import itertools as it ; 
 import os;
 from sys import stdout, stderr, stdin, argv as sysargv;
 import io ;
@@ -10,38 +11,56 @@ import io ;
 wikiextractdir = sysargv[1];
 documentdir    = sysargv[2];
 
-for dirpath, dirsList, filesList in os.walk(wikiextractdir):
-  for filename in filesList:
-    filepath = os.path.join(dirpath, filename);
-    print(filepath, file=stderr);
-    docid = '' ;
-    url   = '' ;
-    title = '' ;
+k = 8 ; 
+maxids = 10**k ; 
+b = 4 ; 
+bsize = 10**b ; 
+
+filePaths = [] ; 
+for dpth, dLs, fLs in os.walk(wikiextractdir):
+  for fn in fLs:
+    fpth = os.path.join(dpth, fn);
+    filePaths.append(fpth) ; 
+
+for fpth in sorted(filePaths) :
+  print(fpth, file=stderr);
+  docid = '' ;
+  url   = '' ;
+  title = '' ;
+  buf = [] ;
+  for line in ru.lines_from_file(fpth):
+    if line.startswith('<doc '):
+      if buf :
+        # remove empty lines at begin and end
+        #buf = [ln for ln in it.dropwhile(lambda X: not X.strip(), buf)] ; 
+        #buf = [ln for ln in it.takewhile(lambda X: X.strip(), buf)] ; 
+        # first line and last line should be specific <doc.* and </doc>
+        buf = buf[1:]   if len(buf) and buf[0].startswith('<doc')  else buf ; 
+        buf = buf[:-1]  if len(buf) and buf[-1].startswith('</doc') else buf ; 
+        if len(buf) and len([l for l in buf if l.strip()]) : 
+          ru.lines_to_file(ofpth, buf) ;
+          print("{0}\t{1}\t{2}".format(docid.zfill(k), title, url), file=stdout) ;
+        buf = [] ;
+      
+      fields  = re.findall('\"[^"]+?\"', line) ;
+      docid   = fields[0][1:-1] ;
+      url     = fields[1][1:-1] ;
+      title   = fields[2][1:-1] ;
+      dirnum  = int(int(docid)/bsize)+1 ;
+      dirname = str(dirnum).zfill(b) ;
+      localdocumentdir = os.path.join(documentdir, dirname) ;
+      if not os.path.isdir(localdocumentdir):
+        os.makedirs(localdocumentdir) ;
+      ofpth = os.path.join(localdocumentdir, 'wiki_{0}'.format(docid.zfill(k))) ;
+    buf.append(line) ;
+  if buf:
+    # remove empty lines at begin and end
+    #buf = [ln for ln in it.dropwhile(lambda X: not X.strip(), buf)] ; 
+    #buf = [ln for ln in it.takewhile(lambda X: X.strip(), buf)] ; 
+    # first line and last line should be specific <doc.* and </doc>
+    buf = buf[1:]   if len(buf) and buf[0].startswith('<doc')  else buf ; 
+    buf = buf[:-1]  if len(buf) and buf[-1].startswith('</doc') else buf ; 
+    if len(buf) and len([l for l in buf if l.strip()]) : 
+      ru.lines_to_file(ofpth, buf) ;
+      print("{0}\t{1}\t{2}".format(docid.zfill(k), title, url), file=stdout) ;
     buf = [] ;
-    for line in ru.lines_from_file(filepath):
-      if line.startswith('<doc'):
-        if buf:
-          buf = buf[1:-1] ;
-          ru.lines_to_file(outfilepath, buf) ;
-          buf = [] ;
-          print("{0}\t{1}\t{2}".format(docid.zfill(6), title, url), file=stdout) ;
-
-        fields = re.findall('\"[^"]+?\"', line) ;
-        docid  = fields[0][1:-1] ;
-        url    = fields[1][1:-1] ;
-        title  = fields[2][1:-1] ;
-
-        dirnum = int(int(docid)/10000) ;
-        dirname = str(dirnum).zfill(2) ;
-        localdocumentdir = os.path.join(documentdir, dirname) ;
-        if not os.path.isdir(localdocumentdir):
-          os.makedirs(localdocumentdir) ;
-        outfilepath = os.path.join(localdocumentdir, 'wiki_{0}'.format(docid.zfill(6))) ;
-      buf.append(line) ;
-
-    if buf:
-      buf = buf[1:-1] ;
-      content = '\n'.join(buf) ;
-      ru.lines_to_file(outfilepath, content) ;
-      buf = [] ;
-
