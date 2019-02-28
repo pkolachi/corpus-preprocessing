45e27fa6f6b68fdba96f6fd68311156d57976eb8
Prasanth Kolachina <prasanth.kolachina@cse.gu.se>
2019-02-28 17:52:16 +0100

    final script that worked for wac corpus; not re-usable

--- scripts/wac/reset_ids.py
+++ scripts/wac/reset_ids.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-from globalimports import * ; 
+from __future__ import print_function ; 
+
+from   globalimports import * ; 
 import random_utils as ru ; 
-from itertools import chain ; 
+import io ; 
+from   itertools import chain ; 
 import sys ; 
 import os.path ; 
 
 DEF = 2 ;
-INFILES = '' if len(sys.argv) < DEF    else sys.argv[:-1] ; 
-OUTFILE = '' if len(sys.argv) < DEF+1  else sys.argv[-1]  ; 
+INFILES = '' if len(sys.argv) < DEF    else sys.argv[1:-1] ; 
+OUTFILE = '' if len(sys.argv) < DEF+1  else sys.argv[-1] ; 
 
 def main_loop() :
   global INFILES ;
@@ -29,10 +32,12 @@ def main_loop() :
   tokens = [] ; 
   pcount = 0  ; 
   scount = 0  ;
-  decerr = 0 ; 
+  decerr = 0  ; 
+  dcount = 0  ;
+  count  = 0  ; 
 
   for inf in INFILES : 
-    with ru.smart_open(inf) as infile : 
+    with ru.smart_open(inf, large=True, fast=True) as infile : 
       for ln in infile : 
         ln = ln.strip() ;
         try : 
@@ -40,7 +45,6 @@ def main_loop() :
         except UnicodeDecodeError : 
           decerr += 1 ; 
           ln = ln.decode('utf-8', errors='replace') ; 
-
         if ln == '# newpar' : 
           # skip this line 
           pcount += 1 ; 
@@ -49,16 +53,18 @@ def main_loop() :
           docid = ln.split('=', 1)[1].strip() ;
           fname = '/'.join(docid.rsplit('/',2)[1:]) ;
           fpref = fname.rsplit('.',1)[0] ; 
-          docm  = '# new_doc  = {0}'.format(docpath+fname) ; 
+          docm  = '# newdoc id = {0}'.format(docpath+fname) ; 
           yield docm ; 
           pcount = 0 ; 
           scount = 0 ; 
+          dcount += 1 ; 
         elif ln.startswith('# text') :
           _, sid = ln.split('=', 1) ;
           newln = '# raw_text = {0}'.format(sid.strip()) ; 
           yield newln ; 
         elif ln.startswith('# sent_id') : 
           scount += 1 ; 
+          count  += 1 ; 
           fnid  = '{0}:{1}:{2}'.format(fpref, pcount, scount) ; 
           yield '# sent_id  = {0}'.format(fnid) ; 
         elif not ln : 
@@ -69,6 +75,7 @@ def main_loop() :
             yield '# tok_text = {0}'.format(tok_text) ; 
             for cn in cbuf :
               yield cn ; 
+              pass ; 
             tokens = [] ; 
             cbuf   = [] ;
           yield ln ; 
@@ -82,10 +89,23 @@ def main_loop() :
               tokens.append(fs[1]) ;
             except ValueError : 
               # this is not a regular node; do not add it to tokens 
+              print(ln.strip(), file=sys.stderr) ; 
               pass ;
           else :
             yield ln ;
+            pass ; 
+
   print("Total number of decoding errors: {0}".format(decerr), file=sys.stderr) ; 
+  #yield dcount ; 
+  #yield count  ; 
 
+#ru.lines_to_file(OUTFILE, main_loop()) ;
+#with io.open(OUTFILE, 'w', encoding='utf-8') as outfile : 
+#  for line in main_loop() :
+#    outfile.write(line+'\n') ; 
 
-ru.lines_to_file(OUTFILE, main_loop()) ;
+import gzip ; 
+with gzip.GzipFile(OUTFILE, mode='wb') as outfile :
+  for line in main_loop() :
+    s = line+'\n';
+    outfile.write(s.encode('utf-8')) ; 
